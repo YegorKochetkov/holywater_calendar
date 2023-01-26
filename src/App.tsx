3c25459043347cbecd510dd0d032d4bd4474b370
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-01-26 19:59:08 +0200

    feat: add components to App
    add DateNavigation & MonthView

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import MonthView from "./components/MonthView/MonthView";
+import styles from "./App.module.css";
+import DateNavigation from "./components/DateNavigation/DateNavigation";
 
 function App() {
-	return <div className="App">App</div>;
+	const [currDate, setCurrDate] = useState(new Date());
+
+	return (
+		<div className={styles.app}>
+			<DateNavigation date={currDate} onChangeDate={setCurrDate} />
+			<MonthView date={currDate} />
+		</div>
+	);
 }
 
 export default App;
