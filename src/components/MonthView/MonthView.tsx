3ecce296e7dc4e9be7e2ca4fcbd8ca8cae25f750
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-01-30 23:57:47 +0200

    feat: update MonthView

--- src/components/MonthView/MonthView.tsx
+++ src/components/MonthView/MonthView.tsx
@@ -1,6 +1,8 @@
-import React, { Fragment, memo, useMemo } from "react";
-import { type EventType } from "../../store";
-import EventShort from "../EventShort/EventShort";
+import React, { memo, useMemo } from "react";
+import useCalendarStore, { type EventType } from "../../store/calendarStore";
+import { findEventsForDay } from "../../utils/findEventsForDay";
+import { toBeginningOfDay } from "../../utils/toBeginningOfDay";
+import EventShort from "../Event/Event";
 import styles from "./MonthView.module.css";
 
 const weeksRows = 6;
@@ -8,34 +10,18 @@ const daysInWeek = 7;
 const dayInMilliseconds = 1000 * 3600 * 24;
 const indexOfMonday = 1;
 
-function toBeginningOfDay(date: Date) {
-	date.setHours(0);
-	date.setMinutes(0);
-	date.setSeconds(0);
-	date.setMilliseconds(0);
-	return date;
-}
-
 type MonthViewProps = {
-	date: Date;
 	events: EventType[];
-	toggleModal: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-function findEventsForDay(events: EventType[], day: Date) {
-	const dateTime = day.getTime();
-	const eventsForDay = events.filter((event) => {
-		const eventFromTime = toBeginningOfDay(new Date(event.from)).getTime();
-		const eventToTime = toBeginningOfDay(new Date(event.to)).getTime();
-
-		return dateTime >= eventFromTime && dateTime <= eventToTime;
-	});
-
-	return eventsForDay;
-}
-
-function MonthView({ date, events, toggleModal }: MonthViewProps) {
+function MonthView({ events }: MonthViewProps) {
 	const currentDay = toBeginningOfDay(new Date());
+	const selectedDate = useCalendarStore((state) => state.selectedDate);
+	const date = useMemo(
+		() => toBeginningOfDay(new Date(selectedDate)),
+		[selectedDate]
+	);
+
 	const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
 
 	const shiftToPrevMonday =
@@ -51,41 +37,40 @@ function MonthView({ date, events, toggleModal }: MonthViewProps) {
 		const dates = [];
 
 		for (let i = 0; i < weeksRows * daysInWeek; i++) {
-			const day = new Date(dayOfMonthView);
+			const day = toBeginningOfDay(new Date(dayOfMonthView));
 			dates.push({ day, events: findEventsForDay(events, day) });
 			dayOfMonthView.setDate(dayOfMonthView.getDate() + 1);
 		}
 
 		return dates;
-	}, [date]);
+	}, [date, events]);
 
 	return (
 		<div className={styles.monthView}>
-			{dates.map((date, index) => (
+			{dates.map((currDate, index) => (
 				<div
 					key={index}
 					className={`
 						${styles.cell}
-						${currentDay.getTime() === date.day.getTime() ? styles.current : ""}
-						${firstDayOfMonth.getMonth() === date.day.getMonth() ? "" : styles.anotherMonth}
+						${currentDay.getTime() === currDate.day.getTime() ? styles.current : ""}
+						${date.getTime() === currDate.day.getTime() ? styles.selected : ""}
+						${
+							firstDayOfMonth.getMonth() === currDate.day.getMonth()
+								? ""
+								: styles.anotherMonth
+						}
 					`}
 				>
 					<div className={styles.date}>
-						<span>{date.day.getDate()}</span>
+						<span>{currDate.day.getDate()}</span>
 						<span>
 							{new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(
-								date.day
+								currDate.day
 							)}
 						</span>
 					</div>
-					{date.events.map((event, index) => (
-						<EventShort
-							key={event.id}
-							id={event.id}
-							showEvent={() => {
-								toggleModal(true);
-							}}
-						/>
+					{currDate.events.map((event) => (
+						<EventShort key={event.id} id={event.id} />
 					))}
 				</div>
 			))}
