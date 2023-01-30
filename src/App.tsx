70771428713ded2a874a19b60282272cd5c7235a
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-01-30 23:30:09 +0200

    feat: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React, { Fragment, useEffect, useState } from "react";
-import MonthView from "./components/MonthView/MonthView";
+import React, { Fragment, useEffect } from "react";
+import MonthView, { mock } from "./components/MonthView/MonthView";
 import styles from "./App.module.css";
 import DateNavigation from "./components/DateNavigation/DateNavigation";
-import useCalendarStore, { type EventType } from "./store";
-import Modal from "./components/Modal/Modal";
-
-const dateForMock = new Date();
-
-const mock: EventType[] = [
-	{
-		id: 1,
-		title: "Mock Event",
-		description:
-			"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum inventore quo vero porro animi ratione, dignissimos incidunt voluptas itaque deserunt id dolorem vitae, doloremque molestiae ipsa consectetur dolor, consequatur corrupti.",
-		from: dateForMock.getTime(),
-		to: dateForMock.getTime(),
-	},
-];
+import useCalendarStore, { type EventType } from "./store/calendarStore";
 
 function App() {
-	const [currDate, setCurrDate] = useState(new Date());
-	const [showModal, setShowModal] = useState(false);
 	const events = useCalendarStore((state) => state.events);
 	const setEvents = useCalendarStore((state) => state.setEvents);
 	const fetchError = useCalendarStore((state) => state.fetchError);
@@ -43,18 +27,9 @@ function App() {
 	return (
 		<Fragment>
 			<div className={styles.app}>
-				<DateNavigation
-					date={currDate}
-					onChangeDate={setCurrDate}
-					toggleModal={setShowModal}
-				/>
-				<MonthView
-					date={currDate}
-					events={events ?? mock}
-					toggleModal={setShowModal}
-				/>
+				<DateNavigation />
+				<MonthView events={events ?? []} />
 			</div>
-			<Modal isOpen={showModal} toggleModal={setShowModal} />
 		</Fragment>
 	);
 }
